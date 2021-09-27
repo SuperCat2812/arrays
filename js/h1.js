428b1c3458c9fd4dc80904c4541da59084e96c54
SuperCat2812 <Sashka281205@gmail.com>
2021-09-27 13:29:38 +0300

    main

---
+++ js/h1.js
@@ -0,0 +1,18 @@
+let mass1 = [1, 2, 3];
+mass1 = push(4, 5, 6);
+mass1.unshift(4, 5, 6);
+console.log(mass1);
+let mass2 = [1, 2, 3, 4, 5];
+let a;
+a = mass2.slice(0, 3);
+a = mass2.slice(3);
+console.log(a);
+
+let mass3 = ['js', 'css', 'jq'];
+let a;
+a = mass3.slice(0, 1);
+console.log(a);
+a=alert(a)
+a = mass3.slice(2);
+console.log(a);
+a=alert(a)
