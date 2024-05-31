947cce8e2aeff2711867014421fac41363a056ec
SuperCat2812 <64689542+SuperCat2812@users.noreply.github.com>
2024-05-31 12:06:55 +0200

    fix

--- js/h1.js
+++ js/h1.js
@@ -1,5 +1,5 @@
 let mass1 = [1, 2, 3];
-mass1 = push(4, 5, 6);
+mass1.push(4, 5, 6);
 mass1.unshift(4, 5, 6);
 console.log(mass1);
 let mass2 = [1, 2, 3, 4, 5];
@@ -9,10 +9,10 @@ a = mass2.slice(3);
 console.log(a);
 
 let mass3 = ['js', 'css', 'jq'];
-let a;
-a = mass3.slice(0, 1);
-console.log(a);
-a=alert(a)
-a = mass3.slice(2);
-console.log(a);
-a=alert(a)
+let b;
+b = mass3.slice(0, 1);
+console.log(b);
+b = alert(b)
+b = mass3.slice(2);
+console.log(b);
+b = alert(b)
